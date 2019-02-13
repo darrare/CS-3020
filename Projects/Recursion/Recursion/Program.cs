35f1d02659200ed97da80a7d1bb2b39095eb662b
Ryan Darras <ryandarras@gmail.com>
2019-02-13 15:06:10 -0700

    asd

--- Projects/Recursion/Recursion/Program.cs
+++ Projects/Recursion/Recursion/Program.cs
@@ -12,7 +12,7 @@ namespace Recursion
         static string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
         //static string[] folderNames = new string[] { "A", "B", "C" };
         static int howDeep = "RYANISCOOL".Length;
-        static int howManyPerLayer = 3;
+        static int howManyPerLayer = 2;
         static string phrase = "RYANISCOOL";
         static string parentName = "FolderTree";
         static Random rand = new Random();
@@ -26,24 +26,21 @@ namespace Recursion
             }
             else
             {
-                FindFile(AppDomain.CurrentDomain.BaseDirectory + "\\" + parentName);
+                FindFile2(AppDomain.CurrentDomain.BaseDirectory + "\\" + parentName);
             }
         }
 
         static bool fileFound = false;
         static void FindFile(string path)
         {
-            if (fileFound)
-                return;
-
             foreach (DirectoryInfo directory in new DirectoryInfo(path).GetDirectories())
             {
                 try
                 {
                     foreach (FileInfo file in new DirectoryInfo(directory.FullName).GetFiles())
                     {
-                        fileFound = true;
                         Console.WriteLine("Found file in " + directory.FullName);
+                        return;
                     }
                     FindFile(directory.FullName);
                 }
@@ -72,6 +69,33 @@ namespace Recursion
                 Directory.CreateDirectory(path + "\\" + s);
                 GenerateDirectory(path + "\\" + s, layer + 1);
             }
+        }
+
+        static void FindFile2(string path)
+        {
+            foreach (DirectoryInfo directory in new DirectoryInfo(path).GetDirectories())
+            {
+                foreach (FileInfo file in new DirectoryInfo(directory.FullName).GetFiles())
+                {
+                    Console.WriteLine("Found file in " + directory.FullName);
+                    return;
+                }
+                FindFile2(directory.FullName);
+            }
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(Factorial(5));
+        //}
+
+        static int Factorial(int val)
+        {
+            if (val == 1)
+            {
+                return val;
+            }
+            return val * Factorial(val - 1);
         }
     }
 }
