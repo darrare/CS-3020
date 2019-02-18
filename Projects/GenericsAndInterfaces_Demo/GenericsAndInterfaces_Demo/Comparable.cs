26c71f8b6b162eb63e75307e90e3cd49ef8bf7f9
darrare <ryandarras@gmail.com>
2019-02-18 12:43:28 -0700

    no message

---
+++ Projects/GenericsAndInterfaces_Demo/GenericsAndInterfaces_Demo/Comparable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndInterfaces_Demo
+{
+    class Currency : IComparable<Currency>
+    {
+        public int gold = 0;
+        public int silver = 0;
+        public int copper = 0;
+
+        public Currency(int gold, int silver, int copper)
+        {
+            this.gold = gold;
+            this.silver = silver;
+            this.copper = copper;
+        }
+
+        private int GetComparableValue()
+        {
+            return gold * 10000 + silver * 100 + copper;
+        }
+
+        public int CompareTo(Currency other)
+        {
+            if (other == null)
+                return 1;
+            else if (GetComparableValue() < other.GetComparableValue())
+                return -1;
+            else if (GetComparableValue() > other.GetComparableValue())
+                return 1;
+            else
+                return 0;
+        }
+
+        public static bool operator > (Currency a, Currency b)
+        {
+            return a.CompareTo(b) == 1;
+        }
+
+        public static bool operator < (Currency a, Currency b)
+        {
+            return a.CompareTo(b) == -1;
+        }
+
+        public static bool operator >= (Currency a, Currency b)
+        {
+            return a.CompareTo(b) >= 0;
+        }
+
+        public static bool operator <= (Currency a, Currency b)
+        {
+            return a.CompareTo(b) <= 0;
+        }
+
+        public static bool operator == (Currency a, Currency b)
+        {
+            return a.CompareTo(b) == 0;
+        }
+
+        public static bool operator != (Currency a, Currency b)
+        {
+            return a.CompareTo(b) != 0;
+        }
+    }
+}
---
+++ Projects/GenericsAndInterfaces_Demo/GenericsAndInterfaces_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GenericsAndInterfaces_Demo
+{
+    class Program
+    {
+        enum Authors { RickAstley, MileyCyrus }
+        static Dictionary<Authors, string> authorDirectories = new Dictionary<Authors, string>();
+
+        static void Main(string[] args)
+        {
+            authorDirectories.Add(Authors.RickAstley, "D:\\Music\\TheBestStuff\\RickAstley\\");
+            authorDirectories.Add(Authors.MileyCyrus, "D:\\Music\\WhyDidMyGFMakeMeDownloadThis\\MileyCyrus\\");
+
+            PlayAuthor(Authors.RickAstley);
+        }
+
+        static void PlayAuthor(Authors author)
+        {
+            foreach (FileInfo audioFile in new DirectoryInfo(authorDirectories[author]).GetFiles("*.wav"))
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(audioFile.FullName);
+                player.Play();
+            }
+            Console.ReadLine();
+        }
+    }
+}
